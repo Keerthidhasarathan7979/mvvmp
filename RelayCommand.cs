c8ec1a0949f3081059da3f74ffda09fd609d8a1a
Keerthidhasarathan7979 <121286975+Keerthidhasarathan7979@users.noreply.github.com>
2023-06-08 20:17:18 +0530

    Add files via upload

---
+++ RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Action execute;
+
+
+        public RelayCommand(Action execute1)
+        {
+            this.execute = execute1;
+        }
+
+
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute.Invoke();
+        }
+    }
+}
